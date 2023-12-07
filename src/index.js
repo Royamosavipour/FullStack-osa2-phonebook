051a4b2a18bd433792c83d7d3a8130bdd7ecc9a8
royamousavi <roya2002_rm@yahoo.com>
2023-12-07 23:37:37 +0200

    ok

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import axios from "axios";
 
-axios.get("http://localhost:3001/persons").then((res) => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById("root")
-  );
-});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
